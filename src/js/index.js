5dbdf53c463d89e66c595b66fbbe3192293e5df0
Nicolle Poltosi <126201109+NicolleMP@users.noreply.github.com>
2024-01-29 15:14:01 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const botao = document.querySelector(".btn-plataform");
+const elementoPlataformas = document.querySelector(".btn-plataform .plataforms");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
+
+
+
+
+
+
